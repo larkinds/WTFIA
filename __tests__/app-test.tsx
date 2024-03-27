10596ca17554c6a0c4079ac6577337080bcce2c0
Larkin <larkindsmith@gmail.com>
2024-03-27 16:56:18 -0400

    Adds a second test

--- __tests__/app-test.tsx
+++ __tests__/app-test.tsx
@@ -6,7 +6,10 @@
 import renderer from 'react-test-renderer';
 import "jest-canvas-mock";
 import App from "../App"
+import { Button } from 'react-native';
 import LocationScreen from '../screens/LocationScreen';
+
+global.setImmediate = jest.useRealTimers as unknown as typeof setImmediate;
 
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
@@ -19,3 +22,21 @@ test('renders correctly', () => {
   const view = renderer.create(<App />).toJSON();
   expect(view).toMatchSnapshot();
 });
+
+test('includes our main button', () => {
+  const root = renderer.create(<App />).root;
+  let button = root.findByType(Button);
+  
+  expect(button.props.title).toEqual("Where the Fuck Am I?")
+});
+
+
+
+
+
+//to test:
+//1. Does button exist?
+//2. Does the application navigate upon clicking the button?
+//3. Does the application return a location?
+//4. Does the application give a nice error if not?
+//do we need to change to react-testing-library?
